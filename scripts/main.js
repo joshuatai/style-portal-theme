de3600f7b98933989cdc6a703489b1cae42bb7d4
Wilson Su <wilson_su@trend.com.tw>
2016-12-05 12:07:33 +0800

    Update selector name in modal callback

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ import parse from './parse-css-ref';
 $(ModalTemplate({ id: 'show-css-ref' })).appendTo('body');
 
 $(document).on('show.bs.modal', '#show-css-ref', function (e) {
-  var $parent = $(e.relatedTarget).parents('.segment-panel');
+  var $parent = $(e.relatedTarget).parents('.segment');
   var title = $parent.data('title');
   var modal = new Modal(e.target);
   var contents = $parent.find('.css-reference').html();
