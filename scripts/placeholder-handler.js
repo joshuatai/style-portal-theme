fcd43570b6fbf034adf1b166253897cbadd21b05
Wilson Su <wilson_su@trend.com.tw>
2016-12-15 16:32:10 +0800

    Add placeholder handler

---
+++ scripts/placeholder-handler.js
@@ -0,0 +1,43 @@
+
+const handler = {};
+
+function image (context, segment) {
+  let $placeholders = $('.placeholder[data-type=image]', context);
+
+  if (!$placeholders.length) { return; }
+
+  $placeholders.each((i, elem) => {
+    let $elem = $(elem);
+    let $image = $('<img>');
+    let data = $elem.data();
+    let width = data.width;
+    let height = data.height;
+    let src = `themes/${route.current.params.theme}/components/${segment.dir}/${data.src}`;
+
+    $elem.css({ width, height });
+    $image.on('load', () => $elem.replaceWith($image)).attr({ src }).addClass(data.class);
+  });
+}
+
+function iframe (context, segment) {
+  let $placeholders = $('.placeholder[data-type=iframe]', context);
+
+  if (!$placeholders.length) { return; }
+
+  $placeholders.each((i, elem) => {
+    let $elem = $(elem);
+    let $iframe = $('<iframe>');
+    let data = $elem.data();
+    let src = `themes/${route.current.params.theme}/components/${segment.dir}/${data.src}`;
+
+    $iframe.attr({ src }).addClass(data.class);
+    $elem.replaceWith($iframe);
+  });
+}
+
+handler.replace = function (context, segment) {
+  image(context, segment);
+  iframe(context, segment);
+};
+
+export default handler;
