b6814939b1fbefdb34f2353b78114f31c6c5d021
Wilson Su <wilson_su@trend.com.tw>
2016-12-27 18:45:33 +0800

    Focus date picker input after date changed
    Set today as default value on date picker.

--- components/date-time-picker/date-time.js
+++ components/date-time-picker/date-time.js
@@ -2,9 +2,13 @@ var container = this;
 var datePickerTime = $('[data-date-picker=date-picker-with-time]', container);
 var timePickerDate = $('[data-time-picker=time-picker-with-date]', container);
 datePickerTime.datepicker({
-	format: 'dd/mm/yyyy',
 	todayHighlight: true
-}).on('show', function(ev){
+})
+.datepicker('setDate', new Date())
+.on('changeDate', function(ev){
+  datePickerTime.focus();
+})
+.on('show', function(ev){
 	datePickerTime.datepicker('iconChange');
 });
 timePickerDate.timeEntry({
