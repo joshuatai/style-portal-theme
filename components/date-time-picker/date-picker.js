95b77965579d6dc2ba33736bbcbe62acf4aead02
Wilson Su <wilson_su@trend.com.tw>
2016-12-27 18:42:57 +0800

    Focus date picker input after date changed

--- components/date-time-picker/date-picker.js
+++ components/date-time-picker/date-picker.js
@@ -1,10 +1,11 @@
 var container = this;
 var datePicker = $('[data-date-picker=date-picker]', container);
-datePicker
-	.datepicker({
-		format: 'dd/mm/yyyy',
-		todayHighlight: true
-	}).on('show', function(ev){
-		console.log("show");
-		datePicker.datepicker('iconChange');
-	});
+
+datePicker.datepicker({
+  todayHighlight: true
+}).on('show', function(ev){
+  console.log("show");
+  datePicker.datepicker('iconChange');
+}).on('changeDate', function(ev){
+  datePicker.focus();
+});
