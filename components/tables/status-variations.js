b94e234dcf1f35386ae2be1e23f7fd8007971aef
ext_austinh <austin_hung@trend.com.tw>
2017-02-16 12:19:36 +0800

    add longtext compoment

--- components/tables/status-variations.js
+++ components/tables/status-variations.js
@@ -1,1 +1,25 @@
 $(".mCustomScrollbar", this).mCustomScrollbar();
+
+var mouseY;
+var mouseX;
+
+var table_cell = $('.table-longtext-truncated').find('tbody > tr').children();
+
+table_cell.mouseover(function(e) {
+	$(this).tooltip({
+		title: 'wef',
+		container: "body",
+		placement: 'right',
+		trigger: 'hover focus',
+		template: "<div class='tooltip' role='tooltip'><div class='tooltip-inner tooltip-inner-light'></div></div>"
+	}).on("mouseover", function (e) {
+		console.log(mouseY);
+		$('.tooltip').css('top', mouseY + 'px')
+		$('.tooltip').css('left', mouseX + 'px')
+	});
+});
+
+$(document).on('mousemove', function(e){
+    mouseY = e.pageY;
+    mouseX = e.pageX;
+})
