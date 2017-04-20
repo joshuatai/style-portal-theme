26f4040b4d6ee935ecb0686455748aef095e54e9
ext_austinh <austin_hung@trend.com.tw>
2017-04-20 14:51:06 +0800

    fixed lock column issue.

--- components/table-fixed/scroll-bar.js
+++ components/table-fixed/scroll-bar.js
@@ -3,3 +3,17 @@ $(".mCustomScrollbar").mCustomScrollbar();
 $(".mCustomHorizontalScrollbar").mCustomScrollbar({
 	axis:"x"
 });  
+
+
+$('.table-horizontal-scrollable .table > tbody > tr').each(function() {
+    var index = $(this).index();
+    var fixedTable = $('.table-horizontal-scrollable.fixed-column .table > tbody > tr');
+    var scrollTable = $('.table-horizontal-scrollable.mCustomHorizontalScrollbar .table > tbody > tr');
+    $(this).hover(function () {
+        fixedTable.eq(index).addClass("hover");
+        scrollTable.eq(index).addClass("hover");
+    },function () {
+        fixedTable.eq(index).removeClass("hover");
+        scrollTable.eq(index).removeClass("hover");
+    });
+});
