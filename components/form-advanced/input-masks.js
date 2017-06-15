0b9d3a0e9b93ddf69b0e27d56cf6cf4d06b81d56
ext_austinh <austin_hung@trend.com.tw>
2017-06-15 18:25:52 +0800

    remove date, time input mask

--- components/form-advanced/input-masks.js
+++ components/form-advanced/input-masks.js
@@ -1,9 +1,3 @@
-$('[data-input-mask=date]', this)
-  .inputmask('mm/dd/yyyy', {showMaskOnFocus: false, showMaskOnHover: false});
-
-$('[data-input-mask=time]', this)
-  .inputmask('hh:mm:ss', {showMaskOnFocus: false, showMaskOnHover: false});
-
 $('[data-input-mask=phone]', this)
   .inputmask('(999) 999-9999', {placeholder: "(000) 000-0000", showMaskOnFocus: false, showMaskOnHover: false});
 
