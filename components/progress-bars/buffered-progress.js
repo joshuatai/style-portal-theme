68b10c0b9445de02b1e7ac7d2ed9b7015273fcd1
Joshua Tai (HIE-TW) <joshua_tai@trend.com.tw>
2017-06-27 05:05:38 -0700

    Fixed issue 381

--- components/progress-bars/buffered-progress.js
+++ components/progress-bars/buffered-progress.js
@@ -4,9 +4,9 @@ var progressbarBuffered = $('.progress-buffered-example .progress-bar-buffered')
 var percentage = $('.progress-buffered-example .progress-bar span');
 
 window.setInterval(function () {
-  var valuenow = Math.floor((progressbar.width()/progress.width()) * 100) + '%';
-  var bufferednow = Math.floor((progressbarBuffered.width()/progress.width()) * 100) + '%';
-  percentage.text(valuenow);
+  var valuenow = Math.floor((progressbar.width()/progress.width()) * 100);
+  var bufferednow = Math.floor((progressbarBuffered.width()/progress.width()) * 100);
+  percentage.text(valuenow + '%');
   progressbar.attr('aria-valuenow', valuenow);
   progressbarBuffered.attr('aria-bufferednow', bufferednow);
 }, 100);
--- components/progress-bars/determinate-progress.js
+++ components/progress-bars/determinate-progress.js
@@ -1,28 +1,6 @@
 var progress = $('.progress-example .progress');
 var progressbar = $('.progress-example .progress-bar');
 var percentage = $('.progress-example .progress-bar span');
-// var flag = false;
-// var setIndeterminate = function () {
-//   percentage.text('0%');
-//   progressbar.attr('aria-valuenow', 0);
-//   setTimeout(setDeterminate, 3000);
-// }
-// var setDeterminate = function () {
-//   var determinateInterval = setInterval(function () {
-//     var valuenow = Math.floor((progressbar.width()/progress.width()) * 100) + '%';
-//     percentage.text(valuenow);
-//     progressbar.attr('aria-valuenow', valuenow);
-//     if (progressbar.width() === progress.width()) {
-//       flag = true;
-//     }
-//     if (progressbar.width() < 50 && flag === true) {
-//       flag = false;
-//       clearInterval(determinateInterval);
-//       setIndeterminate();
-//     }
-//   }, 50);
-// }
-// setDeterminate();
 setInterval(function () {
   var valuenow = Math.floor((progressbar.width()/progress.width()) * 100) + '%';
     percentage.text(valuenow);
