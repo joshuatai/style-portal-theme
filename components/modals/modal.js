61fbec9d4e4806ca6ea9ff762d40b93b37cf14d0
ext_austinh <austin_hung@trend.com.tw>
2017-08-04 15:38:44 +0800

    fix code

--- components/modals/modal.js
+++ components/modals/modal.js
@@ -6,6 +6,10 @@ var modalMd = $('[data-template="modal-md"]');
   
 var modalLg = $('[data-template="modal-lg"]');
 
+var modalMdBody = modalMd.find('modal-body');
+
+var modalLgBody = modalLg.find('modal-body');
+
 $('#demo-xs-button').on('click', function (e){
       $("body").find("#modal-xs").remove("#modal-xs");
       $("body").append(modalXs.clone().attr("id", "modal-xs"));
@@ -15,13 +19,11 @@ $('#demo-sm-button').on('click', function (e){
       $("body").append(modalSm.clone().attr("id", "modal-sm"));
 });
 $('#demo-md-button').on('click', function (e){
-      var modalMdBody = modalMd.find('modal-body');
       $("body").find("#modal-md").remove("#modal-md");
       $("body").append(modalMd.clone().attr("id", "modal-md"));
       modalMdBody.mCustomScrollbar();
 });
 $('#demo-lg-button').on('click', function (e){
-      var modalLgBody = modalLg.find('modal-body');
       $("body").find("#modal-lg").remove("#modal-lg");
       $("body").append(modalLg.clone().attr("id", "modal-lg"));
       modalLgBody.mCustomScrollbar();
