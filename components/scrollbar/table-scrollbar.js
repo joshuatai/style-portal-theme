b06ff229267bcf6f702f77f60aa610d4360e03fb
Austin Hung (HIE-TW) <austin_hung@macde-imac.client.tw.trendnet.org>
2017-09-26 15:06:51 +0800

    Add vh scrollbar

--- components/scrollbar/table-scrollbar.js
+++ components/scrollbar/table-scrollbar.js
@@ -1,23 +1,23 @@
 
-var tableScrollbar = $(".table-scrollbar-example .table-scrollable.mCustomScrollbar");
-var horizontalScrollbar = $(".table-scrollbar-example .mCustomHorizontalScrollbar");
+var tableScrollbar = $(".table-scrollbar-example .table-with-scrollbar.sb-v .table-scrollable");
+var tableHorizontalScrollbar = $(".table-scrollbar-example .table-with-scrollbar.sb-h .table-horizontal-scrollable");
 
 tableScrollbar.mCustomScrollbar();
 
-horizontalScrollbar.mCustomScrollbar({
+tableHorizontalScrollbar.mCustomScrollbar({
     axis:"x"
 });
-$(".mCustomHorizontalScrollbar .mCSB_container").on("mousewheel", function(e){     
+$(".table-with-scrollbar.sb-h .table-horizontal-scrollable .mCSB_container").on("mousewheel", function(e){     
     if(!e.shiftKey) {
         e.preventDefault();
         e.stopPropagation();
     }
 });
 
-$('.table-scrollbar-example .table-horizontal-scrollable .table > tbody > tr').each(function() {
+$('.table-scrollbar-example .table-with-scrollbar.sb-h .table-horizontal-scrollable .table > tbody > tr').each(function() {
     var index = $(this).index();
-    var fixedTable = $('.table-horizontal-scrollable.fixed-column .table > tbody > tr');
-    var scrollTable = $('.table-horizontal-scrollable.mCustomHorizontalScrollbar .table > tbody > tr');
+    var fixedTable = $('.table-with-scrollbar.sb-h .table-horizontal-scrollable.fixed-column .table > tbody > tr');
+    var scrollTable = $('.table-with-scrollbar.sb-h .table-horizontal-scrollable .table > tbody > tr');
     $(this).hover(function () {
         fixedTable.eq(index).addClass("hover");
         scrollTable.eq(index).addClass("hover");
@@ -25,4 +25,16 @@ $('.table-scrollbar-example .table-horizontal-scrollable .table > tbody > tr').e
         fixedTable.eq(index).removeClass("hover");
         scrollTable.eq(index).removeClass("hover");
     });
+});
+
+var sbVerticalHidden = $(".table-scrollbar-example .table-with-scrollbar.sb-vh .table-v-scrollbar-hidden");
+var sbHorizontalHidden = $(".table-scrollbar-example .table-with-scrollbar.sb-vh .table-h-scrollbar-hidden");
+var sbVH = $(".table-scrollbar-example .table-with-scrollbar.sb-vh .table-vh-scrollbar");
+
+sbVerticalHidden.mCustomScrollbar();
+sbHorizontalHidden.mCustomScrollbar({
+    axis:"x"
+});
+sbVH.mCustomScrollbar({
+    axis:"yx"
 });
--- components/table-fixed/scroll-bar.js
+++ components/table-fixed/scroll-bar.js
@@ -1,22 +1,22 @@
 var container = this;
 $(container).children().css('z-index', 2);
 
-$(".mCustomScrollbar").mCustomScrollbar();
+$(".fixed-header-example .table-with-scrollbar.sb-v .table-scrollable").mCustomScrollbar();
 
-$(".mCustomHorizontalScrollbar").mCustomScrollbar({
+$(".fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable").mCustomScrollbar({
     axis:"x"
 }); 
-$(".mCustomHorizontalScrollbar .mCSB_container").on("mousewheel", function(e){     
+$(".fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable .mCSB_container").on("mousewheel", function(e){     
     if(!e.shiftKey) {
         e.preventDefault();
         e.stopPropagation();
     }
 });
 
-$('.table-horizontal-scrollable .table > tbody > tr').each(function() {
+$('.fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable .table > tbody > tr').each(function() {
     var index = $(this).index();
-    var fixedTable = $('.table-horizontal-scrollable.fixed-column .table > tbody > tr');
-    var scrollTable = $('.table-horizontal-scrollable.mCustomHorizontalScrollbar .table > tbody > tr');
+    var fixedTable = $('.fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable.fixed-column .table > tbody > tr');
+    var scrollTable = $('.fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable .table > tbody > tr');
     $(this).hover(function () {
         fixedTable.eq(index).addClass("hover");
         scrollTable.eq(index).addClass("hover");
