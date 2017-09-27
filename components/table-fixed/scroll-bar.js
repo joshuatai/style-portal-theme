269af63d60e476c793f5440e582dca93fc86a38f
Austin Hung <austin_h_hung@trend.com.tw>
2017-09-27 09:11:32 +0800

    sync scrollbar component code

--- components/table-fixed/scroll-bar.js
+++ components/table-fixed/scroll-bar.js
@@ -1,27 +1,35 @@
 var container = this;
 $(container).children().css('z-index', 2);
 
-$(".fixed-header-example .table-with-scrollbar.sb-v .table-scrollable").mCustomScrollbar();
+var scrollbarExample = $('.fixed-header-example');
+var tableScrollbar = $('.table-with-scrollbar.sb-v .table-scrollable', scrollbarExample);
+var tableHorizontalScrollbar = $('.table-with-scrollbar.sb-h .table-horizontal-scrollable', scrollbarExample);
 
-$(".fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable").mCustomScrollbar({
-    axis:"x"
+tableScrollbar.mCustomScrollbar();
+
+tableHorizontalScrollbar.mCustomScrollbar({
+    axis: "x"
 }); 
-$(".fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable .mCSB_container").on("mousewheel", function(e){     
+$('.table-with-scrollbar.sb-h .table-horizontal-scrollable .mCSB_container', scrollbarExample).on('mousewheel', function(e){     
     if(!e.shiftKey) {
         e.preventDefault();
         e.stopPropagation();
     }
 });
 
-$('.fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable .table > tbody > tr').each(function() {
+$('.table-with-scrollbar.sb-h .table > tbody > tr', scrollbarExample).each(function() {
     var index = $(this).index();
-    var fixedTable = $('.fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable.fixed-column .table > tbody > tr');
-    var scrollTable = $('.fixed-header-example .table-with-scrollbar.sb-h .table-horizontal-scrollable .table > tbody > tr');
-    $(this).hover(function () {
-        fixedTable.eq(index).addClass("hover");
-        scrollTable.eq(index).addClass("hover");
-    },function () {
-        fixedTable.eq(index).removeClass("hover");
-        scrollTable.eq(index).removeClass("hover");
-    });
+    var fixedTableRow = $('.table-with-scrollbar.sb-h .fixed-column .table > tbody > tr', scrollbarExample);
+    var scrollTableRow = $('.table-with-scrollbar.sb-h .table-horizontal-scrollable .table > tbody > tr', scrollbarExample);
+    tableSbHover ($(this), index, fixedTableRow, scrollTableRow)
 });
+
+function tableSbHover ($this, index, fixedRow, scrollRow) {
+    $this.hover(function () {
+        fixedRow.eq(index).addClass("hover");
+        scrollRow.eq(index).addClass("hover");
+    },function () {
+        fixedRow.eq(index).removeClass("hover");
+        scrollRow.eq(index).removeClass("hover");
+    });
+}
