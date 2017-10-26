72b36b340ecf944d5179e97a405f9efc67dc3038
Austin Hung <austin_h_hung@trend.com.tw>
2017-10-26 17:33:47 +0800

    Add table single field editing

---
+++ components/table-editable/single-field-editing.js
@@ -0,0 +1,15 @@
+$('.edit-cell > span').editable({
+  mode: 'inline',
+	clear: false,
+  highlight: false,
+  showbuttons: false,
+  inputclass: 'form-control'
+})
+.on('shown', function(e, editable) {
+  var $td = editable.$element.parent();
+  $td.addClass('edit-mode');
+})
+.on('hidden', function(e) {
+  var $td = $(e.target).parent();
+  $td.removeClass('edit-mode');
+});
