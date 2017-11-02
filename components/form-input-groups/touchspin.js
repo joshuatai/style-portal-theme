a081ae43bd5bcac9ec5b44da83958e506929d82b
Austin Hung <austin_h_hung@trend.com.tw>
2017-11-02 15:37:26 +0800

    Add input feature

--- components/form-input-groups/touchspin.js
+++ components/form-input-groups/touchspin.js
@@ -3,33 +3,83 @@ var touchspinHorizontal = $('.touchspin.horizontal');
 var touchspinVerInput = touchspinVertical.find('input');
 var touchspinHorInput = touchspinHorizontal.find('input');
 
-$('[data-touchspin=vertical]', this).mousedown(function(e) {
-  var button = $(this).find('span');
-  if(button.hasClass("icon_angledown")) {
-    touchspinVerInput.val(parseInt(touchspinVerInput.val()) - 1);
-  } 
-  if(button.hasClass("icon_angleup")){
-    touchspinVerInput.val(parseInt(touchspinVerInput.val()) + 1);
-  }
-});
-
-$('[data-touchspin=horizontal]', this).mousedown(function(e) {
-  var button = $(this).find('span');
-  if(button.hasClass("icon-minus")) {
-    touchspinHorInput.val(parseInt(touchspinHorInput.val()) - 1);
-  } 
-  if(button.hasClass("icon-plus")){
-    touchspinHorInput.val(parseInt(touchspinHorInput.val()) + 1);
-  }
-});
-
 $(document).mousedown(function(e) {
   touchspinVertical.removeClass('focused');
   touchspinHorizontal.removeClass('focused');
+  
+  // Add focus class on touchspin wrapper
   if(e.target.closest('.touchspin.vertical')){
     touchspinVertical.addClass('focused');
   }
   if(e.target.closest('.touchspin.horizontal')){
     touchspinHorizontal.addClass('focused');
   }
+  // Edit input value when click button
+  if(e.target.closest('button')) {
+    var button = $(e.target).closest('button').find('span');
+    if(button.hasClass("icon_angleup")){
+      increaseValue(touchspinVerInput);
+    }
+    if(button.hasClass("icon_angledown")) {
+      decreaseValue(touchspinVerInput);
+    } 
+    if(button.hasClass("icon-plus")){
+      increaseValue(touchspinHorInput);
+    }
+    if(button.hasClass("icon-minus")) {
+      decreaseValue(touchspinHorInput);
+    }
+  }
+})
+.on('keydown', function(e) {
+   // Edit input value when up and down key trigger
+  if(e.target.closest('.touchspin.vertical > input')){
+    e.preventDefault();
+    if(e.keyCode === 38) {
+      e.preventDefault();
+      increaseValue(touchspinVerInput);
+    } 
+    if(e.keyCode === 40){
+      e.preventDefault();
+      decreaseValue(touchspinVerInput);
+    }
+  }
+  if(e.target.closest('.touchspin.horizontal > input')){
+    e.preventDefault();
+    if(e.keyCode === 38) {
+      e.preventDefault();
+      increaseValue(touchspinHorInput);
+    } 
+    if(e.keyCode === 40){
+      e.preventDefault();
+      decreaseValue(touchspinHorInput);
+    }
+  }
+})
+.mousewheel(function(e, delta) {
+  // Edit input value when mouse wheel on target input
+  if(e.target.closest('.touchspin.vertical > input')){
+    e.preventDefault();
+    if(delta > 0) {
+      increaseValue(touchspinVerInput);
+    } else {
+      decreaseValue(touchspinVerInput);
+    }
+  }
+  if(e.target.closest('.touchspin.horizontal > input')){
+    e.preventDefault();
+    if(delta > 0) {
+      increaseValue(touchspinHorInput);
+    } else {
+      decreaseValue(touchspinHorInput);
+    }
+  }
 });
+
+function increaseValue(element) {
+  element.val(parseInt(element.val()) + 1);
+}
+
+function decreaseValue(element) {
+  element.val(parseInt(element.val()) - 1);
+}
