498ac797b754d70788a0f759034fa03d67f404ff
Austin Hung <austin_h_hung@trend.com.tw>
2017-11-06 18:27:08 +0800

    implement table column reording

--- components/table-drag-drop/drag-drop-column.js
+++ components/table-drag-drop/drag-drop-column.js
@@ -1,10 +1,10 @@
-var $tableThead = $('.table-column-draggable thead');
-var $tableTbody = $('.table-column-draggable tbody');
+var $tableThead = $('.table-col-draggable-container .table-column-draggable thead');
+var $tableTbody = $('.table-col-draggable-container .table-column-draggable tbody');
 var startIdx;
 var originalIdx;
 
 var fixHelper = function(e, ui) {
-  var tableAlias = $('.table-column-draggable').clone().addClass('draggable-table-col-helper').css({"background-color": "#ffffff", "top": "0px"});
+  var tableAlias = $('.table-col-draggable-container .table-column-draggable').clone().addClass('draggable-table-col-helper').css({"background-color": "#ffffff", "top": "0px"});
   var tableAliasHead = tableAlias.find('thead');
   var tableAliasBody = tableAlias.find('tbody');
   tableAliasHead.html('');
@@ -29,7 +29,6 @@ $tableThead.sortable({
   helper: fixHelper,
   start: function(event, ui) {
     startIdx = $tableThead.find('th').index(ui.item);
-    // Add highlight class on all row element in tbody
     $tableTbody.find('tr').find('td:eq(' + startIdx + ')').each(function() {
         var tdElement = $(this);
         tdElement.addClass('ui-column-highlight');
@@ -37,13 +36,8 @@ $tableThead.sortable({
     originalIdx = startIdx;
   },
   change: function(event, ui) {
-    // Get td index of the placeholder
     var newIdx = $tableThead.find('th').index($tableThead.find('th.ui-column-highlight'));
-
-    // If the position is right of the original position, substract it by one in cause of the 'hidden th'
     if( newIdx > startIdx ) newIdx--;
-
-    // Move all the row elements in tbody
     $tableTbody.find('tr').find('td:eq(' + originalIdx + ')').each(function() {
         var tdElement = $(this);
         var tdElementParent = tdElement.parent();
@@ -57,5 +51,60 @@ $tableThead.sortable({
   },
   stop: function( event, ui ) {
     $tableTbody.find('td').removeClass('ui-column-highlight');
+  }
+});
+
+
+
+var $tableThead2 = $('.table-col-draggable-container-2 .table-column-draggable thead');
+var $tableTbody2 = $('.table-col-draggable-container-2 .table-column-draggable tbody');
+var startIdx2;
+var originalIdx2;
+
+var fixHelper2 = function(e, ui) {
+  var tableAlias = $('.table-col-draggable-container-2 .table-column-draggable').clone().addClass('draggable-table-col-helper2').css({"background-color": "#ffffff", "top": "799px"});
+  var tableAliasHead = tableAlias.find('thead');
+  var tableAliasBody = tableAlias.find('tbody');
+  tableAliasHead.html('');
+  tableAliasBody.html('');
+  tableAliasHead.append(`<tr>${ui.clone().prop('outerHTML')}</tr>`);
+  // tableAliasHead.find('th').width(ui.outerWidth());
+  return tableAlias;
+};
+
+$tableThead2.sortable({
+  axis: "x" ,
+  items: 'th',
+  revert: 300,
+  tolerance: "pointer",
+  cursor: 'move',
+  placeholder: 'ui-column-highlight2',
+  appendTo: ".table-col-draggable-container-2",
+  helper: fixHelper2,
+  start: function(event, ui) {
+    $('.ui-column-highlight2').html(ui.item.prop('innerHTML'));
+    startIdx2 = $tableThead2.find('th').index(ui.item);
+    $tableTbody2.find('tr').find('td:eq(' + startIdx2 + ')').each(function() {
+      var tdElement = $(this);
+      tdElement.addClass('ui-column-highlight2');
+    });
+    originalIdx2 = startIdx2;
+  },
+  change: function(event, ui) {
+    var newIdx = $tableThead2.find('th').index($tableThead2.find('th.ui-column-highlight2'));
+    if( newIdx > startIdx2 ) newIdx--;
+    $tableTbody2.find('tr').find('td:eq(' + originalIdx2 + ')').each(function() {
+        var tdElement = $(this);
+        var tdElementParent = tdElement.parent();
+        if(newIdx > originalIdx2) { // Move it the right
+          tdElementParent.find('td:eq(' + newIdx + ')').after(tdElement); 
+        } else { // Move it the left
+          tdElementParent.find('td:eq(' + newIdx + ')').before(tdElement); 
+        }
+    });
+    originalIdx2 = newIdx;
+  },
+  stop: function( event, ui ) {
+    $tableTbody2.find('td').removeClass('ui-column-highlight2');
   }
 });
