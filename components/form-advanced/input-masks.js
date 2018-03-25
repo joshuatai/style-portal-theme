155f3ca5887290d6f5c9f8719d0f2ae16ae79fbc
Austin <austin_h_hung@trend.com.tw>
2018-03-26 00:56:22 +0800

    implement input mask

--- components/form-advanced/input-masks.js
+++ components/form-advanced/input-masks.js
@@ -8,7 +8,6 @@
       definitions: {
         '0': /\d/,
         'a': /^[A-Za-z]+$/,
-        '*': /./,
         'A': /^[A-Za-z0-9]+$/
       },
       lazy: false,  // make placeholder always visible
@@ -16,19 +15,27 @@
     },
     _create: function () {
       this._renderInputMaskInfo();
+      this._getInputMaskCharData();
       this.mask = new IMask(this.element[0], this.options);
     },
     _renderInputMaskInfo: function () {
       var _self = this;
+      var inputVal;
+      var maskVal;
+      var startPosition;
+      var positionMaskVal;
+      var positionInputVal;
+      var maskRegExp;
+      var selection;
+      var endPosition;
       this.element.on('keydown', function(e) {
         if(e.which === 39){
-          var inputVal = _self.element.val();// user input value
-          var maskVal = _self.options.mask; // 'AA-AAAA-AAAA-AAAA'
-          var startPosition = _self.element[0].selectionStart;
-          
-          var positionMaslVal = maskVal[startPosition]; // A or -(dash)
-          var positionInputVal = inputVal[startPosition]; // user input value or _(underline)
-          var maskRegExp = _self.options.definitions[positionMaslVal]; // **/^[A-Za-z0-9]+$/ or undefined
+          inputVal = _self.element.val();// user input value
+          maskVal = _self.options.mask; // 'AA-AAAA-AAAA-AAAA'
+          startPosition = _self.element[0].selectionStart;
+          positionMaskVal = maskVal[startPosition]; // A or -(dash)
+          positionInputVal = inputVal[startPosition]; // user input value or _(underline)
+          maskRegExp = _self.options.definitions[positionMaskVal]; // **/^[A-Za-z0-9]+$/, /^[A-Za-z]+$/ ..... or undefined
           if(maskRegExp == null) {
             return;
           }
@@ -37,12 +44,58 @@
           }
         }
       });
+    },
+    _getInputMaskCharData: function(){
+      var _self = this;
+      var maskVal = this.options.mask;
+      var filterVal;
+      var charArray = [];
+      var uniqueArray = function(arrArg) {
+        return arrArg.filter(function(elem, pos, arr) {
+          return arr.indexOf(elem) == pos;
+        });
+      };
+      $.each(Object.keys(this.options.definitions), function(index, value) {
+        var regex = new RegExp(value, 'g');
+        if(maskVal.replace(regex, '') !== maskVal) {
+          console.log(maskVal.replace(regex, '').split(''));
+          _self.element.data('regExpChar', uniqueArray(maskVal.replace(regex, '').split('')));
+        }
+      });
+    },
+    alignCursor: function(){
+      this.mask.alignCursor();
     }
+  });
+  $(document).on('mouseup', function(e) {
+    $('input:focus').filter(function(index, elem) {
+      var selection = window.getSelection();
+      return !!$(elem).data('hie-inputMask') && !!String(selection);
+    }).each(function(index, elem) {
+      var selection = window.getSelection();
+      var selectedStr = String(selection);
+      var placeholderChar = $(elem).data('hie-inputMask').mask.masked.placeholderChar;
+      var regExpChar = $(elem).data('regExpChar');
+      var regPre = new RegExp(`^[${placeholderChar} ${regExpChar}]*`); ///^[_ : -.\s\(|\)]*/
+      var regSuf = new RegExp(`[${placeholderChar} ${regExpChar}]*$`); ///[_ : -.\s\(|\)]*$/
+      var countPrefix;
+      var countSuffix;
+      var setRangeStart;
+      var setRangeEnd;
+      countPrefix = selectedStr.length - selectedStr.replace(regPre, '').length;
+      countSuffix = selectedStr.length - selectedStr.replace(regSuf, '').length;
+      setRangeStart = elem.selectionStart + countPrefix;
+      setRangeEnd = elem.selectionEnd - countSuffix;
+      if(countPrefix === countSuffix) {
+        $(elem).inputMask('alignCursor');
+      }
+      elem.setSelectionRange(setRangeStart, setRangeEnd);
+    });
   });
 })(jQuery);
 
 $('[data-input-mask=phone]', this).inputMask({
-  mask: '(00)-0000-0000'
+  mask: '(00) -0000-0000'
 });
 $('[data-input-mask=mobilePhone]', this).inputMask({
   mask: '0000-000-000'
