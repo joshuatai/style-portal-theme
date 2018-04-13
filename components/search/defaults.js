5926d5fba1f6ba115fae74ffbcc157103b10b802
Austin <austin_h_hung@trend.com.tw>
2018-04-13 15:02:11 +0800

    Interaction Flowchart link and fix search box behavior

--- components/search/defaults.js
+++ components/search/defaults.js
@@ -1,3 +1,36 @@
-$('[data-clear-field=clearField]', this).on('click', function (event){
-	$(this).parent().find("input").val("");
-});
+var searchField = $('[data-search-field=searchField]', this);
+searchBox(searchField);
+
+function searchBox(elem) {
+  var $inputGroup = elem;
+  var $loadingIcon = $('<span />').addClass('loader loader-small');
+  var $input = $inputGroup.find('.form-control');
+  var $closeBtn = $inputGroup.find('.tmicon-close-s');
+  var timer = null;
+  $inputGroup.append($loadingIcon);
+  $loadingIcon.hide();
+  $input.on('keyup', function (e){
+    if($(this).val() === '') {
+      $closeBtn.hide();
+    }
+    else {
+      clearTimeout(timer);
+      if($(this).val() !== '') {
+        timer = setTimeout(function(){
+          $closeBtn.hide();
+          $loadingIcon.show();
+          setTimeout(function(){
+            $loadingIcon.hide();
+            $closeBtn.show();
+          }, 800);
+        }, 500);
+      }
+    }
+  });
+  $closeBtn.on('click', function (event){
+    $input.val('');
+    $input.focus();
+    $loadingIcon.hide();
+    $(this).hide();
+  }); 
+}
