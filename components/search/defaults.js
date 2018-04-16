692cb962940d5916718de97bd17b168b07694061
Austin <austin_h_hung@trend.com.tw>
2018-04-16 10:40:23 +0800

    implement search box behavior

--- components/search/defaults.js
+++ components/search/defaults.js
@@ -3,34 +3,89 @@ searchBox(searchField);
 
 function searchBox(elem) {
   var $inputGroup = elem;
-  var $loadingIcon = $('<span />').addClass('loader loader-small');
   var $input = $inputGroup.find('.form-control');
   var $closeBtn = $inputGroup.find('.tmicon-close-s');
-  var timer = null;
-  $inputGroup.append($loadingIcon);
-  $loadingIcon.hide();
   $input.on('keyup', function (e){
-    if($(this).val() === '') {
-      $closeBtn.hide();
-    }
-    else {
-      clearTimeout(timer);
-      if($(this).val() !== '') {
-        timer = setTimeout(function(){
-          $closeBtn.hide();
-          $loadingIcon.show();
-          setTimeout(function(){
-            $loadingIcon.hide();
-            $closeBtn.show();
-          }, 800);
-        }, 500);
-      }
-    }
+    $(this).val() === '' ? $closeBtn.hide() : $closeBtn.show();
   });
   $closeBtn.on('click', function (event){
-    $input.val('');
-    $input.focus();
-    $loadingIcon.hide();
+    $input.val('').focus();
     $(this).hide();
   }); 
 }
+
+
+(function ($) {
+  'use strict';
+  $.widget( 'hie.searchBox', {
+    // default options
+    options: {
+      placeholder: 'Type wording...',
+      value: ''
+    },
+    // The constructor
+    _create: function() {
+      this.timer = null;
+
+      this.$inputIconGroup = $('<div>', {
+        'class': 'input-icon-group'
+      });
+      this.element.wrap(this.$inputIconGroup);
+
+      this.$inputIconLabel = $('<label>', {
+        'class': 'input-icon-label'
+      }).insertAfter(this.element);
+      this.$iconSearch = $('<i>', {
+        'class': 'tmicon tmicon-search-o'
+      }).appendTo(this.$inputIconLabel);
+
+      this.$iconLoader = $('<span>', {
+        'class': 'loader loader-small'
+      }).hide().insertAfter(this.element);
+      this.$iconClose = $('<span>', {
+        'class': 'tmicon tmicon-close-s tmicon-light tmicon-hoverable'
+      }).hide().insertAfter(this.element);
+
+      this.element
+        .on('focus', $.proxy(this.searchBoxFocus, this))
+        .on('blur', $.proxy(this.searchBoxBlur, this))
+        .on('keyup', $.proxy(this.searchBoxKeyUp, this));
+      this.$iconClose
+        .on('click', $.proxy(this.closeIconClick, this));
+    },
+    searchBoxFocus: function(){
+      this.$iconSearch.addClass('focus');
+    },
+    searchBoxBlur: function(){
+      this.$iconSearch.removeClass('focus');
+    },
+    searchBoxKeyUp: function(event) {
+      var _self = this;
+      var selectedLength = String(window.getSelection()).length;
+      var keyCheck = ['Control', 'Meta', 'Shift', 'ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown'].indexOf(event.key);
+      if(this.element.val() === '') {
+        this.$iconClose.hide();
+      }
+      else {
+        clearTimeout(this.timer);
+        if(this.element.val() !== '' && keyCheck === -1 && selectedLength === 0) {
+          this.timer = setTimeout(function(){
+            _self.$iconClose.hide();
+            _self.$iconLoader.show();
+            setTimeout(function(){
+              _self.$iconLoader.hide();
+              _self.$iconClose.show();
+            }, 500);
+          }, 300);
+        }
+      }
+    },
+    closeIconClick: function(event){
+      this.element.val('');
+      this.$iconLoader.hide();
+      this.$iconClose.hide();
+      this.element.focus();
+    }
+  });
+  $('#searchBox').searchBox();
+})(jQuery);
