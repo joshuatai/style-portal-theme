20895b391675b6f85ed50ad8a61a2863b0f8c8e9
Wilson Su <wilson_su@trend.com.tw>
2019-01-15 18:24:46 +0800

    add Advanced Multiple Selection

--- components/form-advanced/multiple-selection.js
+++ components/form-advanced/multiple-selection.js
@@ -1,2 +1,15 @@
 $('[data-multi-selection=multipleSelection]', this).multiSelect();
 $('[data-opt-multi-selection=optMultipleSelection]', this).multiSelect({ selectableOptgroup: true });
+
+const multipleSelectionOptions = {
+  buttonClass: 'btn btn-border',
+  buttonIcons: {
+    addAll: '<span class="tmicon tmicon-collapse-right"></span>',
+    add: '<span class="tmicon tmicon-angle-right"></span>',
+    removeAll: '<span class="tmicon tmicon-collapse-left"></span>',
+    remove: '<span class="tmicon tmicon-angle-left"></span>'
+  },
+};
+
+$('[data-multi-selection=advanced]', this).multipleSelection(multipleSelectionOptions);
+$('[data-opt-multi-selection=advanced]', this).multipleSelection(multipleSelectionOptions);
